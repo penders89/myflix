db0384d0d38c24f9b1c59a72d01fe57139d3140b
penders <robert.pendray@gmail.com>
2017-04-05 16:38:34 +0000

    fix domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Myflix::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'aqueous-headland-41134.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp  
